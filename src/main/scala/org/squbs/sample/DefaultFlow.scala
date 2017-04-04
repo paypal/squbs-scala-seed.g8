7cdc60229c73855dcb4e96ee78c135e44c520465
Sherif Ebady <sebady@paypal.com>
2017-04-04 09:10:31 -0500

    #347 Add pipeline configuration for MetricsFlow
    Add a pipeline configuration for MetricsFlow capture to scala activator.

---
+++ src/main/scala/org/squbs/sample/DefaultFlow.scala
@@ -0,0 +1,16 @@
+package org.squbs.sample
+
+import akka.actor.ActorSystem
+import org.squbs.metrics.MetricsFlow
+import org.squbs.pipeline.{Context, PipelineFlow, PipelineFlowFactory}
+
+/**
+  * Default flow definition that uses squbs provided MetricsFlow
+  */
+class DefaultFlow extends PipelineFlowFactory {
+
+  override def create(context: Context)(implicit system: ActorSystem): PipelineFlow = {
+    MetricsFlow(context.name)
+  }
+
+}
