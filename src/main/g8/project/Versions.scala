d00cc673ccdf09303f96bab29758f24e1c2d1b40
Harikiranvuyyuru <harikiran.vuyyuru@gmail.com>
2017-10-27 07:21:25 -0500

    #524 Upgrade to akka http 10.0.10

--- src/main/g8/project/Versions.scala
+++ src/main/g8/project/Versions.scala
@@ -1,6 +1,6 @@
 object Versions {
   val akkaV = "2.5.4"
-  val akkaHttpV = "10.0.9"
+  val akkaHttpV = "10.0.10"
   val squbsV = "0.10.0-SNAPSHOT"
   val logbackClassicV = "1.1.3"
   val json4sV = "3.5.0"
