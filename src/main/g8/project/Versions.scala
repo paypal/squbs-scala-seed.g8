c96c50ee59be24c537f1d4927fdd535fd5a568f7
Anil Gursel <anilgursel@users.noreply.github.com>
2018-08-08 17:08:00 -0500

    Upgrade to squbs 0.11.0 (#19)

--- src/main/g8/project/Versions.scala
+++ src/main/g8/project/Versions.scala
@@ -1,8 +1,8 @@
 object Versions {
-  val akkaV = "2.5.11"
-  val akkaHttpV = "10.1.0"
-  val squbsV = "0.10.0"
+  val akkaV = "2.5.14"
+  val akkaHttpV = "10.1.3"
+  val squbsV = "0.11.0"
   val logbackClassicV = "1.1.3"
-  val json4sV = "3.5.3"
-  val akkaHttpJson4sV = "1.20.0"
+  val json4sV = "3.5.4"
+  val akkaHttpJson4sV = "1.20.1"
 }
